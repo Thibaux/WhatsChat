0dd88b861d66301183b7d84660cf1694c51d2946
Thibaux Swildens <38690826+Thibaux@users.noreply.github.com>
2022-07-09 15:59:40 +0200

    Created chat messages component

--- frontend/src/components/Chat/subComponents/ChatContent/ChatContent.tsx
+++ frontend/src/components/Chat/subComponents/ChatContent/ChatContent.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import { ChatMessage } from '../ChatMessage/ChatMessage';
 import styles from './chatContent.module.scss';
 
 export const ChatContent = () => {
   return (
     <div className={styles.contentWrapper}>
-      <div className={styles.contentWrapper__chats}>ChatContent</div>
+      <div className={styles.contentWrapper__chats}>
+        <ChatMessage />
+        <ChatMessage />
+      </div>
     </div>
   );
 };
---
+++ frontend/src/components/Chat/subComponents/ChatMessage/ChatMessage.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import styles from './chatMessage.module.scss';
+
+export const ChatMessage = () => {
+  return <div className={styles.messageWrapper}>Some mss</div>;
+};
