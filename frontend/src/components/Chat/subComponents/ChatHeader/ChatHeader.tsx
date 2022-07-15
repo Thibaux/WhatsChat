a8664730b9948209a42623453938e6e1103d2d15
Thibaux Swildens <38690826+Thibaux@users.noreply.github.com>
2022-07-15 18:24:40 +0200

    Added user name in header

--- frontend/src/components/Chat/subComponents/ChatHeader/ChatHeader.tsx
+++ frontend/src/components/Chat/subComponents/ChatHeader/ChatHeader.tsx
@@ -4,10 +4,12 @@ import styles from './chatHeader.module.scss';
 
 export const ChatHeader = () => {
   const { chatName } = useChatStore();
+  const userName = 'Klaas Jan';
 
   return (
     <div className={styles.titleWrapper}>
       <h2>{chatName}</h2>
+      <div className={styles.userName}>{userName}</div>
     </div>
   );
 };
