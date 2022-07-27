0916b3350d221d57c2fb9e09a12e8f0bb653bb0e
Thibaux Swildens <38690826+Thibaux@users.noreply.github.com>
2022-07-27 19:38:09 +0200

    Created messages routes

--- backend/src/Host/Routes/index.ts
+++ backend/src/Host/Routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import userRouter from './UserRouter';
 import chatRouter from './ChatRouter';
+import messagesRouter from './MessagesRouter';
 
 const router = express.Router();
 
 router.use('/users', userRouter);
 router.use('/chat', chatRouter);
+router.use('/messages', messagesRouter);
 
 export default router;
