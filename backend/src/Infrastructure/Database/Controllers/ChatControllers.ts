dc0a1825a360e1811768ae75c032431939185145
Thibaux Swildens <38690826+Thibaux@users.noreply.github.com>
2022-07-27 17:21:12 +0200

    Cleaned controllers

--- backend/src/Infrastructure/Database/Controllers/ChatControllers.ts
+++ backend/src/Infrastructure/Database/Controllers/ChatControllers.ts
@@ -1,43 +1,9 @@
-import { HydratedDocument, QueryWithHelpers, Types } from 'mongoose';
+import { HydratedDocument, Types } from 'mongoose';
 import { JwtPayload } from 'jsonwebtoken';
 import { ChatSchema, MongoUser, UserSchema } from '../GlobalInterfaces';
 import { Chat } from '../Models/Chat';
 import { User } from '../Models/User';
 import { findOneUserById } from './UserControllers';
-
-interface SaveChatReturnI {
-    success: boolean;
-    payload: HydratedDocument<ChatSchema> | string;
-}
-
-export const saveChat = async ({
-    chatTitle,
-    userOne,
-    userTwo,
-}: {
-    chatTitle: string;
-    userOne: MongoUser;
-    userTwo: MongoUser;
-}): Promise<SaveChatReturnI> => {
-    const chat = new Chat({
-        chatTitle,
-        userOne,
-        userTwo,
-    }) as HydratedDocument<ChatSchema>;
-
-    try {
-        await chat.save();
-        return {
-            success: true,
-            payload: chat,
-        };
-    } catch (e) {
-        return {
-            success: false,
-            payload: e.message,
-        };
-    }
-};
 
 interface CreateChatReturnI {
     success: boolean;
@@ -74,35 +40,6 @@ export const createChat = async (
             payload: e.message,
         };
     }
-};
-
-export const updateUsersWithChat = async (
-    userIds: Types.ObjectId[],
-    chat: ChatSchema
-) => {
-    for (const id of userIds) {
-        await User.updateOne(
-            { _id: id },
-            // @ts-ignore
-            { $push: { chats: chat } }
-        );
-    }
-};
-
-export const GetUserByUsername = async (
-    username: string
-): Promise<HydratedDocument<MongoUser>> => {
-    const user = await User.find({ username });
-
-    return user[0] as HydratedDocument<MongoUser>;
-};
-
-export const GetUserByUserId = async (
-    userId: string
-): Promise<HydratedDocument<MongoUser>> => {
-    const user = await User.findById(userId);
-
-    return user as HydratedDocument<MongoUser>;
 };
 
 interface GetAllChatsByUserNameReturnI {
