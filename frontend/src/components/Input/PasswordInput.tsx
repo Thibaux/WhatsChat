08a4135c8e343d416c43e538b9a33d37fe7a42d9
Thibaux Swildens <38690826+Thibaux@users.noreply.github.com>
2022-07-31 19:27:32 +0200

    Created inputs

--- frontend/src/components/Input/PasswordInput.tsx
+++ frontend/src/components/Input/PasswordInput.tsx
@@ -1,6 +1,8 @@
-import React, { ChangeEvent, ChangeEventHandler, useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import {
     Button,
+    FormControl,
+    FormErrorMessage,
     Input,
     InputGroup,
     InputRightElement,
@@ -10,14 +12,19 @@ import {
 interface PasswordInputProps {
     value: string | undefined;
     handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    hasError: boolean;
 }
 
-export const PasswordInput = ({ value, handleChange }: PasswordInputProps) => {
+export const PasswordInput = ({
+    value,
+    handleChange,
+    hasError,
+}: PasswordInputProps) => {
     const [show, setShow] = useState(false);
     const handleClick = () => setShow(!show);
 
     return (
-        <>
+        <FormControl isInvalid={hasError}>
             <Text pb='2' fontWeight='semibold'>
                 Password
             </Text>
@@ -31,12 +38,16 @@ export const PasswordInput = ({ value, handleChange }: PasswordInputProps) => {
                     value={value}
                     onChange={handleChange}
                 />
+
                 <InputRightElement width='4.5rem'>
                     <Button h='1.75rem' size='sm' onClick={handleClick}>
                         {show ? 'Hide' : 'Show'}
                     </Button>
                 </InputRightElement>
             </InputGroup>
-        </>
+            {hasError && (
+                <FormErrorMessage>Password is required.</FormErrorMessage>
+            )}
+        </FormControl>
     );
 };
