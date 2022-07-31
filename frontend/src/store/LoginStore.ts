279c50d9aca1402c462a14590d83c7a703a395ad
Thibaux Swildens <38690826+Thibaux@users.noreply.github.com>
2022-07-31 19:26:37 +0200

    Added login store

--- frontend/src/store/LoginStore.ts
+++ frontend/src/store/LoginStore.ts
@@ -1,10 +1,54 @@
 import create from 'zustand';
+import { devtools } from 'zustand/middleware';
 import produce from 'immer';
+import { postLoginDetails } from '../services/LoginServices';
 
-export interface LoginStoreInterface {
-    chatName: string;
-}
+type LoginStore = {
+    loginFormValues: LoginValues;
+    loginFormErrors: LoginValuesErrors;
+    updateLoginFormValue: ({
+        value,
+        objectName,
+        keyName,
+    }: {
+        value: string | boolean;
+        objectName: string;
+        keyName: string;
+    }) => void;
+    clearLoginErrors: () => void;
+    postLogin: (payload: LoginValues) => void;
+};
 
-export const useLoginStore = create<LoginStoreInterface>((set) => ({
-    chatName: 'default name',
-}));
+export const useLoginStore = create<LoginStore>()(
+    devtools((set) => ({
+        loginFormValues: {
+            emailValue: '',
+            passwordValue: '',
+        },
+        loginFormErrors: {
+            emailValueError: false,
+            passwordValueError: false,
+        },
+        updateLoginFormValue: async ({ value, objectName, keyName }) => {
+            await set(
+                produce((draft) => {
+                    draft[objectName][keyName] = value;
+                })
+            );
+        },
+        clearLoginErrors: () => {
+            set(
+                produce((draft) => {
+                    draft.loginFormErrors = {
+                        emailValueError: false,
+                        passwordValueError: false,
+                        usernameValuesError: false,
+                    };
+                })
+            );
+        },
+        postLogin: async (payload: LoginValues) => {
+            await postLoginDetails(payload);
+        },
+    }))
+);
