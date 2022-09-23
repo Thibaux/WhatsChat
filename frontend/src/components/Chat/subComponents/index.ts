7fc741b882a18585deaa92f8adcbb7c147b23447
Thibaux Swildens <38690826+Thibaux@users.noreply.github.com>
2022-09-23 13:02:24 +0200

    Created an index file for the subComponents

---
+++ frontend/src/components/Chat/subComponents/index.ts
@@ -0,0 +1,6 @@
+export * from './ChatsOverview/ChatsOverview';
+export * from './ChatContent/useChatContent';
+export * from './ChatHeader/ChatHeader';
+export * from './ChatContent/ChatContent';
+export * from './ChatMessage/ChatMessage';
+export * from './ChatInput/ChatInput';
