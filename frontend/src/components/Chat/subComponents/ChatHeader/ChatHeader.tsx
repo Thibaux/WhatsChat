09ce1cd905c8e759bc954b8ae1b566904545533c
Thibaux Swildens <38690826+Thibaux@users.noreply.github.com>
2022-09-26 14:57:49 +0200

    Updated chat header with create chat button

--- frontend/src/components/Chat/subComponents/ChatHeader/ChatHeader.tsx
+++ frontend/src/components/Chat/subComponents/ChatHeader/ChatHeader.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react';
+import { Button, useDisclosure } from '@chakra-ui/react';
 import styles from './chatHeader.module.scss';
 import { useUserStore } from '../../../../store/UserStore';
+import { CreateChatModal } from '../../../UI/Modals';
+import { useChatsStore } from '../../../../store/ChatsStore';
 
 export const ChatHeader = () => {
     const { userObject } = useUserStore();
+    const { handleCreateChat } = useChatsStore();
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     return (
         <div className={styles.titleWrapper}>
+            <Button onClick={onOpen}>Create chat</Button>
+
+            <CreateChatModal
+                isOpen={isOpen}
+                onClose={onClose}
+                handleCreateChat={handleCreateChat}
+            />
+
             <div className={styles.userName}>{userObject.username}</div>
         </div>
     );
--- frontend/src/components/Chat/subComponents/ChatsOverview/ChatsOverview.tsx
+++ frontend/src/components/Chat/subComponents/ChatsOverview/ChatsOverview.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import styles from './chatsOverview.module.scss';
 import { useChatsOverview } from './useChatsOverview';
 import { Header } from '../../../Header/Header';
-import { convertDateTime } from '../../../../utils/convertDateTime';
+import { convertDateTime } from '../../../../utils/converting';
 
 export const ChatsOverview = () => {
     const { chats, handleClickToChat } = useChatsOverview();
@@ -10,18 +10,20 @@ export const ChatsOverview = () => {
     return (
         <div className={styles.chatsOverviewWrapper}>
             <Header />
-            {chats.map(({ _id, chatTitle, updatedAt }) => (
-                <div
-                    id={String(_id)}
-                    key={_id}
-                    className={styles.chatsOverviewWrapper__chatCard}
-                    onClick={() => handleClickToChat(_id, chatTitle)}
-                >
-                    <h3>{chatTitle}</h3>
-                    <p>{convertDateTime(updatedAt)}</p>
-                    <hr />
-                </div>
-            ))}
+            {chats.length === 0
+                ? chats.map(({ _id, chatTitle, updatedAt }) => (
+                      <div
+                          id={String(_id)}
+                          key={_id}
+                          className={styles.chatsOverviewWrapper__chatCard}
+                          onClick={() => handleClickToChat(_id, chatTitle)}
+                      >
+                          <h3>{chatTitle}</h3>
+                          <p>{convertDateTime(updatedAt)}</p>
+                          <hr />
+                      </div>
+                  ))
+                : null}
         </div>
     );
 };
