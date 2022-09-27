3a6fb54b77122850f4ef677313d307e5b637b9e8
Thibaux Swildens <38690826+Thibaux@users.noreply.github.com>
2022-09-27 18:45:53 +0200

    Created receive message func

--- frontend/src/components/Chat/subComponents/ChatContent/useChatContent.ts
+++ frontend/src/components/Chat/subComponents/ChatContent/useChatContent.ts
@@ -1,6 +1,9 @@
 import { useEffect, useRef } from 'react';
+import { SOCKET } from '../../../../utils/Constants';
+import { useMessagesStore } from '../../../../store/MessagesStore';
 
 export const useChatContent = () => {
+    const { updateLocalMessages } = useMessagesStore();
     const scrollIntoViewRef = useRef(null as any);
     const executeScroll = () =>
         scrollIntoViewRef.current?.scrollIntoView({ behavior: 'smooth' });
@@ -8,6 +11,12 @@ export const useChatContent = () => {
     useEffect(() => {
         executeScroll();
     });
+
+    useEffect(() => {
+        SOCKET.on('receive_message', ({ userId, username, message }) => {
+            updateLocalMessages({ userId, username, message });
+        });
+    }, [SOCKET]);
 
     return {
         scrollIntoViewRef,
