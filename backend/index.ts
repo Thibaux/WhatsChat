e42d45a99b0a1f8efc60c58dadd57b7da310d7c5
Thibaux Swildens <38690826+Thibaux@users.noreply.github.com>
2022-09-28 13:55:36 +0200

    Deleted unnecessary files

--- backend/index.ts
+++
@@ -1,7 +0,0 @@
-import completeServer from './src/Host/App';
-
-const server = completeServer.listen(process.env.PORT, () => {
-    console.log('  App is running at http://localhost:%d in %s mode', process.env.PORT, process.env.NODE_ENV);
-});
-
-export default server;
